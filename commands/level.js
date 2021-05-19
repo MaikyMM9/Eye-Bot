0915759905b19025fa9eed29d9d3f08235c851fa
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2021-05-19 18:29:56 +0200

    init
    init

--- commands/level.js
+++ commands/level.js
@@ -1,6 +1,6 @@
 const discord = require("discord.js");
 const fs = require("fs");
-const canvaCord = require("canvaCord")
+// const canvaCord = require("canvaCord")
 
 module.exports.run = async(client, message, args) => {
 
