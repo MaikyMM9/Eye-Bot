4b1588e429117e4c67aa12439133dc6ae7a29bbf
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2021-05-21 12:16:09 +0200

    init
    init

--- commands/welkom.js
+++ commands/welkom.js
@@ -2,6 +2,8 @@ const discord = require("discord.js");
 
 module.exports.run = async(client, message, args) => {
 
+    message.delete();
+
 message.channel.send("Hallo!").then(message.react('✅'))
 
 
