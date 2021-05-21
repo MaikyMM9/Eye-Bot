a82643210e1edb8ea02fcc1b122e85078a9a84b4
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2021-05-21 12:02:15 +0200

    init
    init

---
+++ commands/aankondiging.js
@@ -0,0 +1,41 @@
+const discord = require("discord.js")
+
+module.exports.run = async (client, message, arguments) => {
+
+    message.delete();
+
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(message.author + " Deze commando kun jij helaas niet gebruiken!")
+
+    var role1 = message.guild.roles.cache.find(r => r.id === "843564244821344283");
+
+    var aankondiging = arguments.join(" ")
+
+    var aankondigingEmbed = new discord.MessageEmbed()
+        .setTitle("Aankondiging")
+        .setColor("#86ceb1")
+        .setDescription('Nieuwe aankoniging!')
+        .addField(`De aankondiging:`, aankondiging)
+        .addField(`Verstuurd door:`, message.author)
+        .setFooter("Eye")
+
+
+
+
+    var aankondigingChannel = message.member.guild.channels.cache.find(channels => channels.name === "aankondigingen❗")
+
+    if (!aankondigingChannel) return message.author.send("Geen kanaal gevonden!")
+
+    aankondigingChannel.send(`${role1}`).then(aankondigingChannel.send(aankondigingEmbed))
+
+
+
+
+
+
+
+
+}
+module.exports.help = {
+    name: "aankondiging"
+
+}
