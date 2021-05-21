c0c99bf9a57e8cae4e8d9a6c9e152533ab6cb21f
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2021-05-21 09:33:20 +0200

    init
    init

--- index.js
+++ index.js
@@ -49,6 +49,8 @@ client.on("ready", async () => {
 
 });
 
+var logChannel = message.member.guild.channels.cache.find(channels => channels.name === "staff-logs")
+
 client.on("messageDelete", messageDeleted =>{
 
 if(messageDeleted.author.bot) return;
@@ -63,7 +65,7 @@ var deletedEmbed = new discord.MessageEmbed()
 .setAuthor(`${messageDeleted.author.tag}`, messageDeleted.author.avatarURL({size: 4096}))
 .addField("Het verwijderde bericht:", messageContent)
 
-var logChannel = message.member.guild.channels.cache.find(channels => channels.name === "staff-logs")
+
 logChannel.send(deletedEmbed)
 
 });
