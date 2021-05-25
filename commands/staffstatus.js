e7749756545f86ac0242536ba3db47f7c00caf98
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2021-05-25 14:35:50 +0200

    init
    init

--- commands/staffstatus.js
+++ commands/staffstatus.js
@@ -3,13 +3,27 @@ const discord = require("discord.js");
 module.exports.run = async(client, message, args) => {
 
     
+    var modID = "843564244880195657";
+    var membersWithModRole = message.guild.roles.cache.get(modID).members;
+
+    var helperID = "843564244880195656";
+    var membersWithHelpRole = message.guild.roles.cache.get(helperID).members;
+
+   
+
+   var genoegModerators = membersWithModRole.size == 2
+   var genoegHelpers = membersWithHelpRole.size == 3
+
+
 
 var statusEmbed = new discord.MessageEmbed()
 .setTitle("Status")
 .setTimestamp()
-.setDescription("Dit is de status voor sollicitatie-aanvragen!")
-.addField("Moderators nog nodig:", "0")
-.addField("Helpers nog nodig:", "2")
+.setDescription("Dit is de status voor sollicitatie-aanvragen! Als er genoeg staff leden zijn hoef je niet te solliciteren!")
+.addField("Mensen met de rol moderator:", membersWithModRole)
+addField("Mensen met de rol moderator:", membersWithHelpRole)
+.addField("Moderators nodig:", "3")
+.addField("Helpers nodig:", "4")
 .setFooter("Eye")
 
 message.channel.send(statusEmbed);
