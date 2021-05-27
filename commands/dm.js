c3906e97718bfdc4ae815449e9e2882096981797
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2021-05-27 13:05:36 +0200

    init
    init

--- commands/dm.js
+++ commands/dm.js
@@ -15,19 +15,21 @@ var dmMessage = args.slice(1).join(' ');
 var dmPerson = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
 var userEmbed = new discord.MessageEmbed()
-    .setTitle("**U hebt een bericht verstuurd**")
+    .setTitle(`**${message.author} heeft een bericht verstuurd**`)
     .setColor("#470191")
     .addField("Naar:", dmPerson)
-    .addField("Het bericht dat u heeft verstuurd:", dmMessage)
+    .addField(`Het bericht dat ${message.author} heeft verstuurd:`, dmMessage)
     .setFooter("Eye")
 
 message.author.send(userEmbed)
+var logChannel = message.member.guild.channels.cache.find(channels => channels.name === "staff-logs")
+logChannel.send(userEmbed)
 
 var dmEmbed = new discord.MessageEmbed()
 .setTitle("**U hebt een bericht ontvangen van een Staff-Lid!**")
 .setColor("#470191")
 .addField("Het bericht:" , dmMessage)
-.addField("Dit bericht is niet antwoordbaar", "Dit houd in dat je dit bericht kunt beantwoorden via de Beheerder!")
+.addField("Dit bericht is antwoordbaar", "U kunt dit bericht beantwoorden via de bot!")
 .setFooter("Eye")
 
 dmPerson.send(dmEmbed)
