eb912d56ed8673b6f94ce2b1f1e1c6b558073d75
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2021-05-28 17:22:01 +0200

    init
    init

--- commands/say.js
+++ commands/say.js
@@ -7,8 +7,16 @@ module.exports.run = async (client, message, args) => {
     var botMessage = args.join(" ")
 
     message.delete();
-
+    var sayEmbed = new discord.MessageEmbed()
+        .setTimestamp()
+        .setTitle("De command say is gebruikt")
+        .addField("Door:", message.author)
+        .addField("In:", message.channel)
+        .addField("Het bericht", botMessage)
+        var logChannel = message.member.guild.channels.cache.find(channels => channels.name === "staff-logs")
+        
     message.channel.send(botMessage)
+    logChannel.send(sayEmbed)
 
 
 
