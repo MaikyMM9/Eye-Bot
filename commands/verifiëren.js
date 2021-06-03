be489a3b12ed95d0e6e8d4f9efdf4f364092f205
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2021-06-03 18:01:13 +0200

    init
    init

---
+++ commands/verifiëren.js
@@ -0,0 +1,63 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+
+
+
+    if (message.member.roles.cache.has('849980919573512212')) {
+        return message.channel.send("Je bent al geverifieerd!")
+
+    }
+
+    var role = message.guild.roles.find(role => role.id === "849980919573512212");
+
+
+    var verifiërenembed = new discord.MessageEmbed()
+        .setTimestamp()
+        .setTitle("Weet je zeker dat je jezelf wilt verifiëren?")
+
+
+
+
+
+
+    const embedMessage = await message.channel.send(verifiërenembed);
+
+    embedMessage.react('✅').then(() => embedMessage.react('❌'));
+
+    const filter = (reaction, user) => {
+        return ['✅', '❌'].includes(reaction.emoji.name) && user.id === message.author.id;
+    };
+
+    embedMessage.awaitReactions(filter, { max: 1 })
+        .then(collected => {
+            const reaction = collected.first();
+
+            if (reaction.emoji.name === '✅') {
+                embedMessage.delete();
+                message.member.addRole(role);
+                message.author.send("Je bent nu geverifieerd!")
+                var logChannel = message.member.guild.channels.cache.find(channels => channels.name === "staff-logs")
+                logChannel.send(`${message.author} is nu geverifieerd!`)
+
+            } else if (reaction.emoji.name === '❌') {
+                embedMessage.delete();
+                message.author.send("Je bent niet geverifieerd!")
+
+            }
+        })
+
+
+
+
+
+
+
+
+
+}
+module.exports.help = {
+    name: "verifiëren"
+
+}
