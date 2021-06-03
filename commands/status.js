877ae3154f58290d5ecf187550822326f70b7639
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2021-06-03 20:46:33 +0200

    init
    init

---
+++ commands/status.js
@@ -0,0 +1,30 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    if (!args[0]) return message.reply("Er is geen gebruiker genoemd!");
+
+    var user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+    message.delete();
+
+    var gebruikerEmbed = new discord.MessageEmbed()
+.setTitle("Gegevens")
+.setDescription(`Dit zijn de gegevens van ${user}`)
+.addField(`${user} is gejoind op:`,  message.user.joinedAt)
+.addField(`Wanneer ${user} op discord is aangemeld:`, user.createdAt)
+.addField(`Het ID van ${user}`, user.id)
+
+
+var logChannel = message.member.guild.channels.cache.find(channels => channels.name === "staff-logs")
+   logChannel.send(gebruikerEmbed)
+
+   gebruikerEmbed.delete(60000);
+
+
+
+}
+module.exports.help = {
+    name: "Welkom"
+
+}
