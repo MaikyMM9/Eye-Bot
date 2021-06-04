703d4bd8227ec8ff713cd801990a68d40740976e
MaikyMM9 <51162002+MaikyMM9@users.noreply.github.com>
2021-06-04 16:42:16 +0200

    init
    init

--- commands/status.js
+++ commands/status.js
@@ -19,7 +19,7 @@ module.exports.run = async (client, message, args) => {
 
 
         message.delete();
-        var status = user.member.presence.status;
+        var status = member.user.presence.status;
 
 
         var nickname = user.username;
